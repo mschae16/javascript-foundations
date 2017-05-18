cdf9f9a9a1cb31eed27e5298f88b5c27837a3ba2
Margo Schaedel <margoschaedel@gmail.com>
2017-05-18 11:41:59 -0600

    Finish unicorn test

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,3 +1,12 @@
-function Unicorn() {}
+function Unicorn(name, color='white') {
+  this.name = name;
+  this.color = color;
+  this.isWhite = function() {
+    return isWhite = false;
+  }
+  this.says = function(stuff) {
+    return ('**;* ' + stuff + ' *;**');
+  }
+}
 
 module.exports = Unicorn;
