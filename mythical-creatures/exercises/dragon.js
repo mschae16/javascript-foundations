875ed10fc0bbb27b954f7cbb231656061b2d3cf2
Margo Schaedel <margoschaedel@gmail.com>
2017-05-18 13:00:56 -0600

    Update dragon-test

---
+++ mythical-creatures/exercises/dragon.js
@@ -0,0 +1,17 @@
+function Dragon(name, rider, color) {
+  this.name = name;
+  this.rider = rider;
+  this.color = color;
+  this.hungry = true;
+  // this.eat = function() {
+  //   for(i=0, i<3, i++) {
+  //     if(i<3) {
+  //       return this.hungry = true;
+  //     } else {
+  //       return this.hungry = false;
+  //     }
+  //   }
+  // }
+}
+
+module.exports = Dragon;
